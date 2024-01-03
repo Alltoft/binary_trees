3d69f0acfd2bb3befd69ed8ef2d7b0a65c9f3ba4
Emran <ouakouriimran@gmail.com>
2024-01-03 19:03:36 +0100

    task 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,12 +6,13 @@
  * @func: is a pointer to a function to call for each node.
  * Return: nothing
 */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_inorder(tree->left, func);
-		binary_tree_inorder(tree->right, func);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
 }
