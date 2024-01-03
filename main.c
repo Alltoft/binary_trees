1521b7807d64d3dc6f92822e501c4ee37d4af9fa
Emran <ouakouriimran@gmail.com>
2024-01-03 20:53:07 +0100

    task 9-binary_tree_height.c

--- main.c
+++ main.c
@@ -17,8 +17,6 @@ int main(void)
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
-    binary_tree_insert_left(root->right, 8);
-    binary_tree_insert_left(root->right->right, 12);
     binary_tree_print(root);
 
     height = binary_tree_height(root);
